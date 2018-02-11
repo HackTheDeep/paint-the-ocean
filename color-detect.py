d9057586f9bf36686b7e6584ae2c8259178e0cd5
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-11 10:34:02 -0500

    fix style for color-detect.py

--- color-detect.py
+++ color-detect.py
@@ -8,35 +8,32 @@ lowerBound = np.array([0, 170, 90])
 upperBound = np.array([378, 255, 255])
 
 cam = cv2.VideoCapture('final_stablizied_stitched_video.mpg')
-kernelOpen=np.ones((5,5))
-kernelClose=np.ones((20,20))
-
-# font=cv2.cv.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX,2,0.5,0,3,1)
+kernelOpen = np.ones((5, 5))
+kernelClose = np.ones((20, 20))
 
 while(cam.isOpened()):
-    ret, img=cam.read()
-    img=cv2.resize(img,(340,220))
+    ret, img = cam.read()
+    img = cv2.resize(img, (680, 440))
 
-    #convert BGR to HSV
-    imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    # convert BGR to HSV
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # create the Mask
-    mask=cv2.inRange(imgHSV,lowerBound,upperBound)
+    mask = cv2.inRange(imgHSV, lowerBound, upperBound)
 
-    #morphology
-    maskOpen=cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
-    maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+    # morphology
+    maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
+    maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
 
-    maskFinal=maskClose
-    conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    maskFinal = maskClose
+    conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_NONE)
 
-    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    cv2.drawContours(img, conts, -1, (255, 0, 0), 3)
     for i in range(len(conts)):
-        x,y,w,h=cv2.boundingRect(conts[i])
+        x, y, w, h = cv2.boundingRect(conts[i])
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255))
-        # cv2.cv.PutText(cv2.cv.fromarray(img), str(i+1),(x,y+h),font,(0,255,255))
-    cv2.imshow("maskClose",maskClose)
-    cv2.imshow("maskOpen",maskOpen)
-    cv2.imshow("mask",mask)
-    cv2.imshow("cam",img)
+    cv2.imshow("maskClose", maskClose)
+    cv2.imshow("cam", img)
+    cv2.imshow("imgHSV", imgHSV)
     cv2.waitKey(10)
