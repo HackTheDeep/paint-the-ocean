a846b915b2ea2b3c94582a3dbb4a09ce93d9f493
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-11 03:12:55 -0500

    update test.py

--- test.py
+++ test.py
@@ -4,19 +4,25 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.gridspec as gridspec
 import glob
+import os
 import cv2
 
+def create_result(image, title):
+    # Plot the result
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    #f.tight_layout()
+    ax1.axis('off')
+    ax1.imshow(image)
+    ax1.set_title('Original', fontsize=18)
+    ax2.axis('off')
+    ax2.imshow(result)
+    ax2.set_title('Edges', fontsize=18)
+    plt.savefig(title)
 
-image = mpimg.imread('img/img-0451.JPG')
-result = get_edges(image, separate_channels=True)
 
-# Plot the result
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-#f.tight_layout()
-ax1.axis('off')
-ax1.imshow(image)
-ax1.set_title('Original', fontsize=18)
-ax2.axis('off')
-ax2.imshow(result)
-ax2.set_title('Edges', fontsize=18)
-plt.savefig("result.jpg")
+os.chdir('img/')
+for filename in os.listdir(os.getcwd()):
+    image = mpimg.imread(filename)
+    result = get_edges(image, separate_channels=True)
+    title = filename + ".jpg"
+    create_result(image, title)
