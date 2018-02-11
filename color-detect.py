f631ba75160e6c39794289d672cb68d5cc6acadc
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-11 04:58:43 -0500

    fix style for color-detect.py

--- color-detect.py
+++ color-detect.py
@@ -12,18 +12,19 @@ while(cam.isOpened()):
     ret, img = cam.read()
     img = cv2.resize(img, (340, 220))
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(imgHSV,lowerBound,upperBound)
-    kernelOpen = np.ones((5,5))
-    kernelClose = np.ones((20,20))
-    maskOpen = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
-    maskClose = cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+    mask = cv2.inRange(imgHSV, lowerBound, upperBound)
+    kernelOpen = np.ones((5, 5))
+    kernelClose = np.ones((20, 20))
+    maskOpen = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernelOpen)
+    maskClose = cv2.morphologyEx(maskOpen, cv2.MORPH_CLOSE, kernelClose)
 
-    maskFinal=maskClose
-    conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    maskFinal = maskClose
+    conts, h = cv2.findContours(maskFinal.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img, conts, -1, (255, 0, 0), 3)
 
-    cv2.imshow("maskClose",maskClose)
-    cv2.imshow("maskOpen",maskOpen)
+    cv2.imshow("maskClose", maskClose)
+    cv2.imshow("maskOpen", maskOpen)
 
     # cv2.imshow('imgHSV', imgHSV)
     # cv2.imshow('img', img)
